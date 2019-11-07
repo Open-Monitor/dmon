03e0d7ca765eaeaa22589c215de006712a7837d7
GuyARoss <guyallenross@gmail.com>
2019-11-07 17:11:34 -0500

    reorg file structure.

---
+++ host-monitor/lib/rpc-handlers/index.js
@@ -0,0 +1,1 @@
+export {default as transmitHandler} from './transmit-handler';
--- host-monitor/src/transmit-handler.js
+++
@@ -1,8 +0,0 @@
-export default ({request}, callback) => {
-  console.log(request, callback);
-
-  callback(null, {
-    didInsert: true,
-    frequencyAdjustment: 0,
-  });
-};
