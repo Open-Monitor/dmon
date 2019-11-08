6ea94bd14c5c09e93bd35b59279a3b462120a707
GuyARoss <guyallenross@gmail.com>
2019-11-07 20:45:42 -0500

    added elastic search in manifest.

--- host-monitor/config/manifest.js
+++ host-monitor/config/manifest.js
@@ -3,6 +3,10 @@ const manifest = (env) => ({
     dev: 'localhost:50051',
     prod: 'localhost:50051',
   })[env],
+  elastic: ({
+    dev: 'http://localhost:9200',
+    prod: 'http://localhost:9200',
+  })[env],
 });
 
 export default manifest(
