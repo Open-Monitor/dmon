238f8162356034ad0bc0f53110704b641a5b4d47
GuyARoss <guyallenross@gmail.com>
2019-11-09 20:45:56 -0500

    added logger to manifest.

--- host-monitor/config/manifest.js
+++ host-monitor/config/manifest.js
@@ -1,11 +1,17 @@
+import {prodLogger, devLogger} from '../logger';
+
 const manifest = (env) => ({
   grpc: ({
-    dev: '192.168.1.101:50486',
+    dev: 'localhost:50486',
     prod: '192.168.1.101:50486',
   })[env],
   elastic: ({
     dev: 'http://localhost:9200',
     prod: 'http://localhost:9200',
+  })[env],
+  logger: ({
+    dev: devLogger,
+    prod: prodLogger,
   })[env],
 });
 
