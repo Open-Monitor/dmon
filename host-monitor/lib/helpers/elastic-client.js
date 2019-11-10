d4efafa0bbc690d4fa387317bab17658c803bfea
GuyARoss <guyallenross@gmail.com>
2019-11-09 20:47:13 -0500

    moved out the elastic client logic to new file.

---
+++ host-monitor/lib/helpers/elastic-client.js
@@ -0,0 +1,22 @@
+import manifest from '../../config/manifest';
+
+import {Client} from '@elastic/elasticsearch';
+import {CLIENT_FAILED} from '../constants/elastic';
+
+const client = new Client({node: manifest.elastic});
+
+export const index = async (index, request) => {
+  try {
+    const resp = await client.index({
+      index: index,
+      body: {
+        ...request,
+      },
+    });
+
+    return resp.body.result;
+  } catch (e) {
+    manifest.logger('error', e);
+    return CLIENT_FAILED;
+  }
+};
