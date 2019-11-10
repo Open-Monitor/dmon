7f1f228c2980bd10cf73804d12526586fb530773
GuyARoss <guyallenross@gmail.com>
2019-11-09 20:47:22 -0500

    created live ws handler.

---
+++ host-monitor/lib/ws-handlers/live.js
@@ -0,0 +1,24 @@
+import uuid from 'uuid';
+
+import {liveSocketPool} from '../bridge';
+
+export default async (socket) => {
+  const connectionInstance = uuid.v4();
+
+  socket.on('subscribeToLiveTransmission', async (ips) => {
+    liveSocketPool.register(
+        connectionInstance,
+        ips,
+        (data) => {
+          socket.emit(
+              'liveTransmission',
+              data,
+          );
+        });
+  });
+
+  socket.on(
+      'disconnect',
+      () => liveSocketPool.unregister(connectionInstance),
+  );
+};
