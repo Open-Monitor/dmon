dbd43d568a8a1b97dd1437663def1c3dce1f1298
GuyARoss <guyallenross@gmail.com>
2019-11-09 20:56:10 -0500

    fixed logging lib.

--- host-monitor/logger/dev.js
+++ host-monitor/logger/dev.js
@@ -1,7 +1,9 @@
 import * as Types from './types';
 
 export default (type, action) => ({
+  [true]: () => console.log(action),
   [Types.WARING]: () => console.warn(action),
   [Types.LOG]: () => console.log(action),
   [Types.WARING]: () => console.warn(action),
-})[type];
+})[type]();
+;
