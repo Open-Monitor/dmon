4e21f23ad9c9d8eeafabeff868655bfc680238a9
GuyARoss <guyallenross@gmail.com>
2019-11-11 00:49:15 -0500

    added this stuff for dumb

--- host-monitor/config/manifest.js
+++ host-monitor/config/manifest.js
@@ -2,16 +2,19 @@ import {prodLogger, devLogger} from '../logger';
 
 const manifest = (env) => ({
   grpc: ({
-    dev: '192.168.1.101:50486',
-    prod: '192.168.1.101:50486',
+    dev: 'localhost:50486',
+    prod: 'localhost:50486',
+    dumbshit: '192.168.1.101:50486',
   })[env],
   elastic: ({
     dev: 'http://localhost:9200',
     prod: 'http://localhost:9200',
+    dumbshit: 'http://localhost:9200',
   })[env],
   logger: ({
     dev: devLogger,
     prod: prodLogger,
+    dumbshit: devLogger,
   })[env],
 });
 
